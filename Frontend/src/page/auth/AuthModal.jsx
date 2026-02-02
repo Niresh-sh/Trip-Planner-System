31683588b61c97e9374234ddc1bb7aafd9f97913
Niresh-sh <neerace.sh@gmail.com>
2026-02-02 17:20:58 +0545

    updated auth

--- Frontend/src/page/auth/AuthModal.jsx
+++ Frontend/src/page/auth/AuthModal.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import axios from "axios";
-import registerService from "../../services/RegisterService";
+import registerService from "../../Services/RegisterService";
 import loginImage from "../../assets/login.png";
 import { GoogleLogin } from "@react-oauth/google";
 
