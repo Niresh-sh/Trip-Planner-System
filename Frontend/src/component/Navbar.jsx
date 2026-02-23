2fcf573ea5b87c8befc42d79a9153d80e38f3170
Niresh-sh <neerace.sh@gmail.com>
2026-02-23 12:37:04 +0545

    modified navbar

--- Frontend/src/component/Navbar.jsx
+++ Frontend/src/component/Navbar.jsx
@@ -207,21 +207,6 @@ function Navbar({ openLoginModal }) {
                   )}
                   
                   <span className="sr-only">Open main menu</span>
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    ariahidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
                 </button>
               </div>
             </div>
